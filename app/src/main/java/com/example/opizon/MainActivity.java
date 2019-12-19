dc3f4a2f009b38be4f75839981d450c4733323ea
GhassenBenMakhlouf <GhassenBenMakhlouf@gmail.com>
2019-12-19 21:07:49 +0100

    GBM: access to the layout of the 2 apps with basic speech recognition in app 1

--- app/src/main/java/com/example/opizon/MainActivity.java
+++ app/src/main/java/com/example/opizon/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.opizon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView app1Card, app2Card;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        app1Card = (CardView) findViewById(R.id.app1_card);
+        app2Card = (CardView) findViewById(R.id.app2_card);
+        app1Card.setOnClickListener(this);
+        app2Card.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.app1_card : i = new Intent(this,App1.class); startActivity(i); break;
+            case R.id.app2_card : i = new Intent(this,App2.class); startActivity(i); break;
+            default : break;
+        }
+
     }
 }
